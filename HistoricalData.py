6406bde41fad556cbf5042128f6b371785c4a5ef
Jonathan W <jwilliams12117@gmail.com>
2022-06-29 19:56:16 -0700

    Added querystrings and API calls for multiple datasets, modified Main.py to take that data for its calculation

--- HistoricalData.py
+++ HistoricalData.py
@@ -1,11 +1,15 @@
 import requests
 import json
+import datetime
 
 # Base URL for the API
 url = "https://yh-finance.p.rapidapi.com/stock/v3/get-historical-data"
 
 # Querystring parameters
-querystring = {"symbol":"VTSAX"}
+querystring_VTSAX = {"symbol":"VTSAX"}
+querystring_VTIAX = {"symbol":"VTIAX"}
+querystring_VBTLX = {"symbol":"VBTLX"}
+querystring_VTABX = {"symbol":"VTABX"}
 
 # API Key and headers
 headers = {
@@ -13,18 +17,56 @@ headers = {
 	"X-RapidAPI-Host": "yh-finance.p.rapidapi.com"
 }
 
-res = requests.get(url, headers=headers, params=querystring)
+res_VTSAX = requests.get(url, headers=headers, params=querystring_VTSAX)
+res_VTIAX = requests.get(url, headers=headers, params=querystring_VTIAX)
+res_VBTLX = requests.get(url, headers=headers, params=querystring_VBTLX)
+res_VTABX = requests.get(url, headers=headers, params=querystring_VTABX)
 
 # Parse the JSON API response
-data = res.json()
+data_VTSAX = res_VTSAX.json()
+data_VTIAX = res_VTIAX.json()
+data_VBTLX = res_VBTLX.json()
+data_VTABX = res_VTABX.json()
 
 
-# Print out the results
-for item in data["prices"]:
-    if "adjclose" in item:
-        print("\n\nItem: ", item, "\n")
-        print("Adjusted Close: ", item["adjclose"])
-    else:
-        print("No price data")
+# function to convert dates to readable format from UNIX time
+def convertTime(unix_date):
+    date = datetime.datetime.fromtimestamp(unix_date)
+    month = date.strftime("%b")
+    day = date.strftime("%d")
+    year = date.strftime("%Y")
+    print("Date: "+month+" "+day+", "+year)
+    return str(month+" "+day+", "+year)
+    
+    
+# Array to hold adjusted closing prices and their associated dates
+historical_data_VTSAX = []
+historical_data_VTIAX = []
+historical_data_VBTLX = []
+historical_data_VTABX = []
 
+# Append the items in a response to an array
+def fill_array(response_array, target_array):
+	for item in response_array["prices"]:
+		if "adjclose" in item:
+			# For now, we will only append the price.  The date may come in handy later.
+			# target_array.append([convertTime(item["date"]), item["adjclose"]])
+			target_array.append(item["adjclose"])
+        
+
+# Run the function on each of the API responses
+fill_array(data_VTSAX, historical_data_VTSAX)
+fill_array(data_VTIAX, historical_data_VTIAX)
+fill_array(data_VBTLX, historical_data_VBTLX)
+fill_array(data_VTABX, historical_data_VTABX)
+
+
+# Modified array to pass to Main.py which just contains prices
+price_data = []
+# for item in historical_data:
+#     if (item[0] != "No price data"):
+#     	price_data.append(item[1])
+    
+
+# print("\n\nPrices only: ", price_data[:30])
 
--- Main.py
+++ Main.py
@@ -1,8 +1,10 @@
+import HistoricalData
+
 # Define array to hold output return data
 iteration_results = []
 
 # Set cash_out_duration for use in the functions
-cash_out_duration = 3
+cash_out_duration = 90
 
 # Define beginning and ending date ranges in UNIX time and as array-iterable integers
 begin_unix = 1483228800
@@ -12,11 +14,15 @@ date_begin = 0
 date_end = 1376
 
 # Organize Data Arrays
-VTSAX_historical_data = [92.1, 91.1, 91.57, 89.48, 89.1, 92.32, 90.94, 91.25, 95.15, 98.04, 100.47, 101.58, 100.54, 100.21]
-VTIAX_historical_data = [27.39, 27.48, 27.77, 27.36, 27.78, 28.41, 27.97, 28.09, 29.09, 29.68, 30.32, 30.53, 30.46, 30.31]
-VBTLX_historical_data = [9.87, 9.84, 9.76, 9.8, 9.8, 9.77, 9.67, 9.73, 9.89, 9.97, 10.01, 9.98, 10.04, 10.06]
-VTABX_historical_data = [20.01, 19.71, 19.56, 19.42, 19.56, 19.51, 19.58, 19.36, 19.51, 19.74, 19.84, 19.91, 19.98, 19.94]
-VMFFX_historical_data = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+VTSAX_historical_data = HistoricalData.historical_data_VTSAX
+VTIAX_historical_data = HistoricalData.historical_data_VTIAX
+VBTLX_historical_data = HistoricalData.historical_data_VBTLX
+VTABX_historical_data = HistoricalData.historical_data_VTABX
+VMFXX_historical_data = []
+
+# Fill VMFXX with $1 entries for the length of the other arrays
+for entry in range(len(VTSAX_historical_data)):
+    VMFXX_historical_data.append(1)
 
 
 # Define beginning excluded dates
@@ -30,14 +36,14 @@ VTSAX_weight = 0.36
 VTIAX_weight = 0.24
 VBTLX_weight = 0.28
 VTABX_weight = 0.12
-VMFFX_weight = 0.00
+VMFXX_weight = 0.00
 
 # Define starting portfolio balance
 beginning_balance = 100000
 
 
 # Function that will compute weighted portfolio change
-def portfolio_change(date_index, VTSAX_weight, VTIAX_weight, VBTLX_weight, VTABX_weight, VMFFX_weight):
+def portfolio_change(date_index, VTSAX_weight, VTIAX_weight, VBTLX_weight, VTABX_weight, VMFXX_weight):
 	# For each asset in the portfolio, compute the weighted change by subtracting the price at the current index - the price at the previous index
 	# from the corresponding historical data array and dividing by the prior day change, then multiplyingf by the weight
 
@@ -45,10 +51,10 @@ def portfolio_change(date_index, VTSAX_weight, VTIAX_weight, VBTLX_weight, VTABX
     VTIAX_change = VTIAX_weight * ((VTIAX_historical_data[date_index] - VTIAX_historical_data[date_index - 1]) / VTIAX_historical_data[date_index - 1])
     VBTLX_change = VBTLX_weight * ((VBTLX_historical_data[date_index] - VBTLX_historical_data[date_index - 1]) / VBTLX_historical_data[date_index - 1])
     VTABX_change = VTABX_weight * ((VTABX_historical_data[date_index] - VTABX_historical_data[date_index - 1]) / VTABX_historical_data[date_index - 1])
-    VMFFX_change = VMFFX_weight * ((VMFFX_historical_data[date_index] - VMFFX_historical_data[date_index - 1]) / VMFFX_historical_data[date_index - 1])
+    VMFXX_change = VMFXX_weight * ((VMFXX_historical_data[date_index] - VMFXX_historical_data[date_index - 1]) / VMFXX_historical_data[date_index - 1])
     
     # Sum the percent change and return
-    portfolio_chg = VTSAX_change + VTIAX_change + VBTLX_change + VTABX_change + VMFFX_change
+    portfolio_chg = VTSAX_change + VTIAX_change + VBTLX_change + VTABX_change + VMFXX_change
     
     return portfolio_chg
 
@@ -78,17 +84,17 @@ def iterate_return():
             VTIAX_weight = 0
             VBTLX_weight = 0
             VTABX_weight = 0
-            VMFFX_weight = 0   
+            VMFXX_weight = 0   
         else:
             VTSAX_weight = 0.36
             VTIAX_weight = 0.24
             VBTLX_weight = 0.28
             VTABX_weight = 0.12
-            VMFFX_weight = 0.00
+            VMFXX_weight = 0.00
         
         # Run portfolio_change for each date, excluding date 0
         if (day != 0):
-            day_change = portfolio_change(day, VTSAX_weight, VTIAX_weight, VBTLX_weight, VTABX_weight, VMFFX_weight)
+            day_change = portfolio_change(day, VTSAX_weight, VTIAX_weight, VBTLX_weight, VTABX_weight, VMFXX_weight)
             
             # Add or subtract that change from the prior day's balance
             balance = round(balance * (1 + day_change), 2)
@@ -110,7 +116,7 @@ iteration_results.append(base_case_HPR)
 print("Base Case:", iteration_results)
 
 # Reset the cash out duration
-cash_out_duration = 3
+cash_out_duration = 90
 
 # Loop through iterate_return, progressing the excluded dates and appending to iteration_results,
 # as long as exclusion_date doesn't exceed the dataset range
